d593c5c067cf9dac38638a263bf35cdf566da625
Mumi <543528285@qq.com>
2021-02-22 18:15:32 +0800

    feat:完成输入功能

--- src/component/TodoInput/index.jsx
+++ src/component/TodoInput/index.jsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 
-import './index.less'
+import './index.scss'
 
 export default class TodoInput extends Component {
     constructor(props) {
@@ -10,11 +10,31 @@ export default class TodoInput extends Component {
         }
     }
 
+    // 监听值的改变
+    handleChangeValue = ({target:{value}}) => {
+        this.setState({inputValue:value})
+    }
+
+    handleAddValue = () => {
+        const { inputValue } = this.state
+        // e.stopPropagation()
+        // alert(this.state.inputValue)
+        if(inputValue.trim() === '') return alert('请输入待办')
+        // TODO:传值到父组件 并清除值
+        this.props.addData(inputValue)
+        // console.log(this.props.addData(inputValue))
+        this.inputValue = ''
+
+    }
+
+
+
+
     render(){
         return (
             <div className='todoinput-container'>
-                <input type="text" className='todoinput-container' />
-                <div className='submit'>添加待办</div>
+                <input type="text" className='todoinput' onInput={this.handleChangeValue} />
+                <div className='submit' onClick={this.handleAddValue}>添加待办</div>
             </div>
         )
     }
--- src/page/TodoList/index.jsx
+++ src/page/TodoList/index.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import TodoInput from "../../component/TodoInput";
 
+import './index.scss'
+
 
 export default class TodoList extends Component {
     constructor(props) {
@@ -10,11 +12,19 @@ export default class TodoList extends Component {
         }
     }
 
+    addData = (value) => {
+        const todoItem = {
+            value,
+            id: new Date().getTime()
+        } 
+        this.setState({list:[...this.state.list,todoItem]})
+    }
+
     render(){
         return (
             <>
                 <h1>ToDoList</h1>
-                <TodoInput />
+                <TodoInput  addData={this.addData} />
             </>
         )
     }
